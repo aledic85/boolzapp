d4ebfacb97a4b9bb48f77eb8307b58fa0eb1ea78
aledic85 <dicuollo85@gmail.com>
2019-03-12 12:31:32 +0100

    half of the third milestone completed

--- script.js
+++ script.js
@@ -10,18 +10,21 @@ function feedGenerator() {
 
 function sendMessage(input) {
 
-	var wrapper = $(".wrapper");
+	var wrapper = $(".wrapper.active");
 
 	var messageContSend = document.createElement("div");
 	var wrapperSend = document.createElement("div");
 	var message = document.createElement("p");
+	var span = document.createElement("span");
 
 	$(messageContSend).addClass("message-cont-send");
 	$(wrapperSend).addClass("wrapper-send");
 
 	$(message).text(input);
+	$(span).text("17.30");
 
-	wrapperSend.append(message)
+	wrapperSend.append(message);
+	wrapperSend.append(span);
 	messageContSend.append(wrapperSend);
 
 	wrapper.append(messageContSend);
@@ -29,18 +32,21 @@ function sendMessage(input) {
 
 function receivedMessage() {
 
-	var wrapper = $(".wrapper");
+	var wrapper = $(".wrapper.active");
 
 	var messageContReceived = document.createElement("div");
 	var wrapperReceived = document.createElement("div");
 	var message = document.createElement("p");
+	var span = document.createElement("span");
 
 	$(messageContReceived).addClass("message-cont-received");
 	$(wrapperReceived).addClass("wrapper-rec");
 
 	$(message).text(feedGenerator);
+	$(span).text("17.30");
 
-	wrapperReceived.append(message)
+	wrapperReceived.append(message);
+	wrapperReceived.append(span);
 	messageContReceived.append(wrapperReceived);
 
 	wrapper.append(messageContReceived);
@@ -62,11 +68,51 @@ function textEnter(e) {
 	}
 }
 
+function userClick() {
+
+	var me = $(this);
+	meInd = me.index();
+	var wrapAct = $(".active");
+
+	wrapAct.removeClass("active");
+
+	var wrappers = $(".right-cont > .wrapper");
+	var nextWrap = wrappers.eq(meInd);
+
+	nextWrap.addClass("active")
+}
+
+function userSearch() {
+
+	var me = $(this);
+	var val = me.val();
+	var users = $(".user > .header");
+
+	users.removeClass("hidden");
+	users.removeClass("height");
+
+	for (var i = 0; i < users.length; i++) {
+
+		var user = users.eq(i);
+		var userCont = user.text();
+
+		if (!userCont.includes(val)) {
+
+			user.addClass("hidden");
+			users.addClass("height");
+		}
+	}
+}
+
 function init() {
 
 	var textBox = $("#text-box");
+	var user = $(".user > .header");
+	var search = $("#search");
 
 	textBox.keyup(textEnter);
+	user.click(userClick);
+	search.keyup(userSearch)
 }
 
 $(document).ready(init)
